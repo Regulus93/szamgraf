c62ce0f7a47f79da2c63e6ce35e947104a9866c4
Bicsak Dani <bicsak.dani@gmail.com>
2020-05-23 13:07:30 +0200

    Added initial start project

---
+++ classes/Practice/Initial_project/Beadando_2/Beadando_2/MyApp.cpp
@@ -0,0 +1,213 @@
+#include "MyApp.h"
+
+#include <math.h>
+#include <vector>
+
+#include <array>
+#include <list>
+#include <tuple>
+#include <imgui/imgui.h>
+#include "includes/GLUtils.hpp"
+
+CMyApp::CMyApp(void)
+{
+	m_camera.SetView(glm::vec3(5, 5, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+}
+
+CMyApp::~CMyApp(void)
+{
+}
+
+void CMyApp::InitSkyBox()
+{
+	m_SkyboxPos.BufferData(
+		std::vector<glm::vec3>{
+		// hátsó lap
+		glm::vec3(-1, -1, -1),
+		glm::vec3(1, -1, -1),
+		glm::vec3(1, 1, -1),
+		glm::vec3(-1, 1, -1),
+		// elülsõ lap
+		glm::vec3(-1, -1, 1),
+		glm::vec3(1, -1, 1),
+		glm::vec3(1, 1, 1),
+		glm::vec3(-1, 1, 1),
+	}
+	);
+
+	// és a primitíveket alkotó csúcspontok indexei (az elõzõ tömbökbõl) - triangle list-el való kirajzolásra felkészülve
+	m_SkyboxIndices.BufferData(
+		std::vector<int>{
+			// hátsó lap
+			0, 1, 2,
+			2, 3, 0,
+			// elülsõ lap
+			4, 6, 5,
+			6, 4, 7,
+			// bal
+			0, 3, 4,
+			4, 3, 7,
+			// jobb
+			1, 5, 2,
+			5, 6, 2,
+			// alsó
+			1, 0, 4,
+			1, 4, 5,
+			// felsõ
+			3, 2, 6,
+			3, 6, 7,
+	}
+	);
+
+	// geometria VAO-ban való regisztrálása
+	m_SkyboxVao.Init(
+		{
+			{ CreateAttribute<0, glm::vec3, 0, sizeof(glm::vec3)>, m_SkyboxPos },
+		}, m_SkyboxIndices
+	);
+
+	// skybox texture
+	glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
+
+	glGenTextures(1, &m_skyboxTexture);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, m_skyboxTexture);
+
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+	TextureFromFileAttach("assets/xpos.png", GL_TEXTURE_CUBE_MAP_POSITIVE_X);
+	TextureFromFileAttach("assets/xneg.png", GL_TEXTURE_CUBE_MAP_NEGATIVE_X);
+	TextureFromFileAttach("assets/ypos.png", GL_TEXTURE_CUBE_MAP_POSITIVE_Y);
+	TextureFromFileAttach("assets/yneg.png", GL_TEXTURE_CUBE_MAP_NEGATIVE_Y);
+	TextureFromFileAttach("assets/zpos.png", GL_TEXTURE_CUBE_MAP_POSITIVE_Z);
+	TextureFromFileAttach("assets/zneg.png", GL_TEXTURE_CUBE_MAP_NEGATIVE_Z);
+
+	glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+
+}
+
+void CMyApp::InitShaders()
+{
+
+	m_programSkybox.Init(
+		{
+			{ GL_VERTEX_SHADER, "myVert.vert"},
+			{ GL_FRAGMENT_SHADER, "myFrag.frag"}
+		},
+		{
+			{ 0, "vs_in_pos" }				// VAO 0-as csatorna menjen a vs_in_pos-ba
+		}
+	);
+
+	// shader program rövid létrehozása, egyetlen függvényhívással a fenti három:
+	m_programSkybox.Init(
+		{
+			{ GL_VERTEX_SHADER, "skybox.vert" },
+			{ GL_FRAGMENT_SHADER, "skybox.frag" }
+		},
+		{
+			{ 0, "vs_in_pos" },				// VAO 0-as csatorna menjen a vs_in_pos-ba
+		}
+	);
+}
+
+bool CMyApp::Init()
+{
+	glClearColor(0.125f, 0.25f, 0.5f, 1.0f);
+
+	glEnable(GL_CULL_FACE); 
+	glEnable(GL_DEPTH_TEST);
+
+	InitShaders();
+	InitSkyBox();
+	
+	// kamera
+	m_camera.SetProj(45.0f, 640.0f / 480.0f, 0.01f, 1000.0f);
+
+	return true;
+}
+
+void CMyApp::Clean()
+{
+	glDeleteTextures(1, &m_skyboxTexture);
+
+}
+
+void CMyApp::Update()
+{
+	static Uint32 last_time = SDL_GetTicks();
+	float delta_time = (SDL_GetTicks() - last_time) / 1000.0f;
+
+	m_camera.Update(delta_time);
+
+	last_time = SDL_GetTicks();
+}
+
+void CMyApp::Render()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glm::mat4 viewProj = m_camera.GetViewProj();
+
+	m_program.Use();
+
+	m_program.Unuse();
+
+	// skybox
+	GLint prevDepthFnc;
+	glGetIntegerv(GL_DEPTH_FUNC, &prevDepthFnc);
+
+	glDepthFunc(GL_LEQUAL);
+
+	m_SkyboxVao.Bind();
+	m_programSkybox.Use();
+	m_programSkybox.SetUniform("MVP", viewProj * glm::translate( m_camera.GetEye()) );
+	
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, m_skyboxTexture);
+	glUniform1i(m_programSkybox.GetLocation("skyboxTexture"), 0);
+
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
+	m_programSkybox.Unuse();
+
+	glDepthFunc(prevDepthFnc);
+
+	ImGui::ShowTestWindow();
+}
+
+void CMyApp::KeyboardDown(SDL_KeyboardEvent& key)
+{
+	m_camera.KeyboardDown(key);
+}
+
+void CMyApp::KeyboardUp(SDL_KeyboardEvent& key)
+{
+	m_camera.KeyboardUp(key);
+}
+
+void CMyApp::MouseMove(SDL_MouseMotionEvent& mouse)
+{
+	m_camera.MouseMove(mouse);
+}
+
+void CMyApp::MouseDown(SDL_MouseButtonEvent& mouse)
+{
+}
+
+void CMyApp::MouseUp(SDL_MouseButtonEvent& mouse)
+{
+}
+
+void CMyApp::MouseWheel(SDL_MouseWheelEvent& wheel)
+{
+}
+
+void CMyApp::Resize(int _w, int _h)
+{
+	glViewport(0, 0, _w, _h );
+
+	m_camera.Resize(_w, _h);
+}
---
+++ classes/Practice/Initial_project/Beadando_2/Beadando_2/myVert.vert
@@ -0,0 +1,18 @@
+#version 330 core
+
+// VBO-ból érkezõ változók
+in vec3 vs_in_pos;
+
+// a pipeline-ban tovább adandó értékek
+out vec3 vs_out_pos;
+
+// shader külsõ paraméterei
+uniform mat4 MVP;
+uniform mat4 world;
+
+void main()
+{
+	gl_Position = MVP * vec4( vs_in_pos, 1 );
+	
+	vs_out_pos = (world * vec4(vs_in_pos, 1)).xyz;
+}
